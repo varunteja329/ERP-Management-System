07d99966bef32cd503704130f711f0a5f782947a
NIMMAKURI VARUN TEJA <150577693+varunteja329@users.noreply.github.com>
2026-07-21 17:51:44 +0530

    Add files via upload

---
+++ Database/01_CreateDatabase.sql.sql
@@ -0,0 +1,13 @@
+-- ==========================================
+-- Project : ERP Management System
+-- Module  : Database Creation
+-- Author  : Varun Teja
+-- Version : 1.0
+-- Date    : 21-07-2026
+-- ==========================================
+
+DROP DATABASE IF EXISTS ERP_DB;
+
+CREATE DATABASE ERP_DB;
+
+USE ERP_DB;
